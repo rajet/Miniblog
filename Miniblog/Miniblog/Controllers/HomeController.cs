e42fca5dd4b5a86b15be9237e40eda84066ff37b
keerthdi <r.keerthdi@gmail.com>
2018-01-16 15:11:00 +0100

    added sms nexmo auth

--- Miniblog/Miniblog/Controllers/HomeController.cs
+++ Miniblog/Miniblog/Controllers/HomeController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using System.Text;
+using System.IO;
+using System.Data.SqlClient;
 
 namespace Miniblog.Controllers
 {
@@ -15,9 +19,90 @@ namespace Miniblog.Controllers
 
         public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
+            // In order to make this code work -> replace all UPPERCASE-Placeholders with the corresponding data!
+            var username = Request["username"];
+            var password = Request["password"];
+
+            var mode = "SMS";
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\miniblog.mdf\";Integrated Security=True;";
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT [Id], [username], [password], [phonenumber] FROM [dbo].[User] WHERE [username] = '" + username + "' AND [password] = '" + password + "'";
+            cmd.Connection = con;
+
+            con.Open();
+
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    if (username == reader["username"].ToString() && password == reader["password"].ToString())
+                    {
+                        var request = (HttpWebRequest)WebRequest.Create("https://rest.nexmo.com/sms/json");
+
+                        var secret = "TEST_SECRET";
+
+                        var postData = "api_key=1cb5b15d";
+                        postData += "&api_secret=ea21d1dbbd4f86d4";
+                        postData += "&to=" + reader["phonenumber"];
+                        postData += "&from=\"\"Miniblog\"\"";
+                        postData += "&text=\"" + secret + "\"";
+                        var data = Encoding.ASCII.GetBytes(postData);
+
+                        request.Method = "POST";
+                        request.ContentType = "application/x-www-form-urlencoded";
+                        request.ContentLength = data.Length;
+
+                        using (var stream = request.GetRequestStream())
+                        {
+                            stream.Write(data, 0, data.Length);
+                        }
+
+                        var response = (HttpWebResponse)request.GetResponse();
+                        var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                        ViewBag.Message = responseString;
+
+
+                        //Check token
+                        TokenLogin();
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Wrong Credentials";
+                    }
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Username or password is wrong!";
+            }
 
             return View();
+        }
+
+
+
+        [HttpPost]
+        public void TokenLogin()
+        {
+            var token = Request["token"];
+
+            if (token == "TEST_SECRET")
+            {
+                // -> "Token is correct";
+            }
+            else
+            {
+                // -> "Wrong Token";
+            }
+
         }
 
         public ActionResult Contact()
