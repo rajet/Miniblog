48d51ff58ebe5cdb46ffcf4f63484464784bf340
keerthdi <r.keerthdi@gmail.com>
2018-01-17 13:31:28 +0100

    added logout function

--- Miniblog/Miniblog/Controllers/HomeController.cs
+++ Miniblog/Miniblog/Controllers/HomeController.cs
@@ -13,6 +13,8 @@ namespace Miniblog.Controllers
 {
     public class HomeController : Controller
     {
+        private string connectionString = @"Data Source=(local);AttachDbFilename=D:\db\localdb\Data\miniblog.mdf;database=miniblog;Integrated Security=True;";
+
         public ActionResult Index()
         {
             return View();
@@ -43,12 +45,13 @@ namespace Miniblog.Controllers
             int userId = 0;
 
             SqlConnection con = new SqlConnection();
-            con.ConnectionString = @"Data Source=.\MSSQLSERVER;AttachDbFilename=\App_Data\miniblog.mdf;Integrated Security=True;";
+            con.ConnectionString = connectionString;
+            //con.ConnectionString = @"Data Source=(local);AttachDbFilename=|DataDirectory|\miniblog.mdf;database=miniblog;Integrated Security=True;";
 
             SqlCommand cmd = new SqlCommand();
             SqlDataReader reader;
 
-            cmd.CommandText = "SELECT [id], [username], [password], [phonenumber] FROM [dbo].[User] WHERE [username] = '" + username + "' AND [password] = '" + password + "'";
+            cmd.CommandText = "SELECT [Id], [Username], [Password], [Phonenumber] FROM [dbo].[User] WHERE [Username] = '" + username + "' AND [Password] = '" + password + "'";
             cmd.Connection = con;
 
             con.Open();
@@ -59,9 +62,9 @@ namespace Miniblog.Controllers
             {
                 while (reader.Read())
                 {
-                    if (username == reader["username"].ToString() && password == reader["password"].ToString())
+                    if (username == reader["Username"].ToString() && password == reader["Password"].ToString())
                     {
-                        userId = Convert.ToInt32(reader["id"]);
+                        userId = Convert.ToInt32(reader["Id"]);
                         var request = (HttpWebRequest)WebRequest.Create("https://rest.nexmo.com/sms/json");
 
                         byte[] time = BitConverter.GetBytes(DateTime.UtcNow.ToBinary());
@@ -70,12 +73,12 @@ namespace Miniblog.Controllers
 
                         string expiry = DateTime.Now.AddMinutes(5).ToString();
 
-                        cmd.CommandText = "INSERT INTO [dbo].[Token] (user_id, tokenstring, expiry) VALUES('" + userId + "', '" + secret + "', '" + expiry + "')";
+                        cmd.CommandText = "INSERT INTO [dbo].[Token] (User_id, Tokenstring, Expiry) VALUES ('" + userId + "', '" + secret + "', '" + expiry + "')";
                         cmd.ExecuteNonQuery();
 
                         var postData = "api_key=1cb5b15d";
                         postData += "&api_secret=ea21d1dbbd4f86d4";
-                        postData += "&to=" + reader["phonenumber"];
+                        postData += "&to=" + reader["Phonenumber"];
                         postData += "&from=\"\"Miniblog\"\"";
                         postData += "&text=\"" + secret + "\"";
                         var data = Encoding.ASCII.GetBytes(postData);
@@ -106,6 +109,8 @@ namespace Miniblog.Controllers
                 ViewBag.Message = "Username or password is wrong!";
             }
 
+            con.Close();
+
             return RedirectToAction("Home", "SMS_Auth", new { userId = userId, username = username });
         }
 
@@ -113,12 +118,12 @@ namespace Miniblog.Controllers
         public ActionResult SMS_Auth(int userId, string username)
         {
             SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\miniblog.mdf\";Integrated Security=True;";
+            con.ConnectionString = connectionString;
 
             SqlCommand cmd = new SqlCommand();
             SqlDataReader reader;
 
-            cmd.CommandText = "SELECT [Id], [username], [password], [phonenumber] FROM [dbo].[Token] WHERE [user_id] = '" + userId + "'";
+            cmd.CommandText = "SELECT [Id], [Username], [Password], [Phonenumber] FROM [dbo].[Token] WHERE [User_id] = '" + userId + "'";
             cmd.Connection = con;
 
             con.Open();
@@ -129,10 +134,10 @@ namespace Miniblog.Controllers
             {
                 while (reader.Read())
                 {
-                    if (Convert.ToDateTime(reader["expriy"]) > DateTime.Now)
+                    if (Convert.ToDateTime(reader["Expriy"]) > DateTime.Now)
                     {
                         string sms_key = Request["sms_key"];
-                        string secret = reader["tokenstring"].ToString();
+                        string secret = reader["Tokenstring"].ToString();
 
                         if (sms_key == secret)
                         {
@@ -153,7 +158,41 @@ namespace Miniblog.Controllers
                     }
                 }
             }
+
+            con.Close();
+
             return View();
+        }
+
+        public ActionResult Logout()
+        {
+            string userId = Session["userId"].ToString();
+            string sessionId = Session.SessionID;
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = connectionString;
+
+            SqlCommand cmd = new SqlCommand();
+
+            //entry in userlogin table
+            cmd.CommandText = "UPDATE [dbo].[Userlogin] SET Deletedon = '" + DateTime.Now.ToString() + "' WHERE User_id = '" + userId + "' AND Sessionid = '" + sessionId + "'";
+            cmd.Connection = con;
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO [dbo].[Userlog] (User_id, Action) VALUES ('" + userId + "', '" + DateTime.Now.ToString() + ": logout')";
+            cmd.Connection = con;
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            //destroy sessions
+            Session.Clear();
+            Session.Abandon();
+
+            return RedirectToAction("Home", "Login");
         }
 
         public ActionResult Contact()
